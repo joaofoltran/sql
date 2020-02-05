89755029c9c172c01e8b205b89a31cf860427bdc
João Foltran <fooltran@gmail.com>
2020-02-05 10:14:55 -0300

    removed backup job filtering and formatted a few columns in sched_jobs script

--- rman_jobs.sql
+++ rman_jobs.sql
@@ -5,6 +5,6 @@ SELECT SESSION_KEY, INPUT_TYPE, STATUS,
        TO_CHAR(END_TIME,'mm/dd/yy hh24:mi')   end_time,
        ELAPSED_SECONDS/3600                   hrs
 FROM V$RMAN_BACKUP_JOB_DETAILS
-WHERE INPUT_TYPE in ('ARCHIVELOG','DB FULL')
-and start_time > sysdate - interval '15' day
+--WHERE INPUT_TYPE in ('ARCHIVELOG','DB FULL')
+WHERE start_time > sysdate - interval '15' day
 ORDER BY SESSION_KEY;
--- user_sched_jobs.sql
+++ user_sched_jobs.sql
@@ -1,15 +1,18 @@
-col job_name for a35
-col job_action for a50
-col repeat_interval for a50
-col last_start_date for a35
-col next_run_date for a35
+col job_name for a30
+col job_action for a40
+col interv for a30
+col last_start_date for a30
+col next_run_date for a30
+col fail format '9'
+col runs format '9999'
+
 
 select job_name, 
 job_action, 
-repeat_interval,
+repeat_interval as interv,
 enabled,
-run_count,
-failure_count,
+run_count as runs,
+failure_count as fail,
 last_start_date,
 next_run_date
 from user_scheduler_jobs;
